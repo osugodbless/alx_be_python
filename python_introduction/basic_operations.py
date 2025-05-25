00fe04c20661eba7e1fe458cc9cb35f5b868f6d2
osugodbless <osugodbless234@gmail.com>
2025-05-25 23:16:51 +0100

    Added a .py file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+# Perform addition, substrcaction and multiplication operation and store the results
+summation = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+# Print all results
+print("Addition of", number1, "and", number2, "is", summation)
+print("Substraction of", number1, "and", number2, "is", difference)
+print("Multiplication of", number1, "and", number2, "is", product)
