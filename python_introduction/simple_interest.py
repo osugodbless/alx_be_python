8fb42c26267708b62c3cf57b6d53edc033167408
osugodbless <osugodbless234@gmail.com>
2025-05-25 23:31:09 +0100

    Added python file for calculating simple interest earned

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,7 @@
+principal = 1000
+rate = 0.05
+time = 3
+#Calculate the simple interest earned
+interest = principal * rate * time
+# Print the calculated interest
+print("The simple interest is:", interest)
