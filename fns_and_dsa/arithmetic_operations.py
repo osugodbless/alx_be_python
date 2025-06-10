2d7b30bb4706619623e7050c57f14a3e2c45aa39
osugodbless <osugodbless234@gmail.com>
2025-06-11 00:20:31 +0100

    module for main.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,12 +1,15 @@
 def perform_operation(num1, num2, operation):
     if operation == "add":
         result = num1 + num2
-    elif operation == "substract":
+    elif operation == "subtract":
         result = num1 - num2
     elif operation == "multiply":
         result = num1 * num2
     elif operation == "divide":
-        result = num1 / num2
+        if num2 == 0:
+            result = f"{num1} cannot be divided by zero(0). Zero is an invalid divisor, please enter a valid number."
+        else:
+            result = num1 / num2
     else:
         result = "You entered an invalid operation. Please try again!"
     return result
