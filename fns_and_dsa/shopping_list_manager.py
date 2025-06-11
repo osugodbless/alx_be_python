579c48a11cb4aacc7f7384657019ee5e27084a59
osugodbless <osugodbless234@gmail.com>
2025-06-11 15:26:11 +0100

    performing list operations

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,35 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            item = input("Please enter the item name: ").lower()
+            shopping_list.append(item)
+            print(f"{item} has been added to your shopping list")
+        elif choice == '2':
+            item = input("Enter the item name to remove: ").lower()
+            if item in shopping_list:
+                shopping_list.remove(item)
+                print(f"{item} removed successfully")
+            else:
+                print(f"Item not in your shopping list")
+        elif choice == '3':
+            for item in shopping_list:
+                print(item)
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
