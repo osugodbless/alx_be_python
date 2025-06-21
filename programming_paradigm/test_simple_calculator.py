21214be4a8a935c8411b2f371ecaacf93c20f072
osugodbless <osugodbless234@gmail.com>
2025-06-21 10:06:03 +0100

    basics of unit testing

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -12,21 +12,21 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
 
-    def test_substraction(self):
-        """Test the substraction method."""
+    def test_subtraction(self):
+        """Test the subtraction method."""
         self.assertEqual(self.calc.subtract(2, 3), -1)
         self.assertEqual(self.calc.subtract(-2, 3), -5)
         self.assertEqual(self.calc.subtract(-2, -3), 1)
         self.assertEqual(self.calc.subtract(5, 2), 3)
 
-    def test_multiplication(self):
+    def test_multiply(self):
         """Test the multiplication method."""
         self.assertEqual(self.calc.multiply(10, 0), 0)
         self.assertEqual(self.calc.multiply(-10, -2), 20)
         self.assertEqual(self.calc.multiply(-10, 1), -10)
         self.assertEqual(self.calc.multiply(10, 5), 50)
 
-    def test_division(self):
+    def test_divide(self):
         """Test the division method."""
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(-10, 2), -5)
